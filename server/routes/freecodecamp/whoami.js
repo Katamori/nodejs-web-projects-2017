d47cdab9535a78021faa7c20c66f66ce33528213
Katamori <zol.sch93@gmail.com>
2017-05-24 19:13:31 +0200

    FCC/whoami reverted. No better solution for IP

--- server/routes/freecodecamp/whoami.js
+++ server/routes/freecodecamp/whoami.js
@@ -2,10 +2,9 @@
 module.exports = {
         method: 'GET',
         path: '/freecodecamp/whoami',
-        handler: function (request, reply) { 
-            console.log(Object.keys(request.connection._connections)[0].split(":")[0]);   
+        handler: function (request, reply) {   
             reply({
-                ipadress_proper: Object.keys(request.connection._connections)[0].split(":")[0], 
+                ipaddress: request.info.remoteAddress, 
                 language: request.headers["accept-language"].split(",")[0],
                 software: request.headers["user-agent"]
             });
